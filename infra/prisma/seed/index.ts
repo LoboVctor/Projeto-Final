b6b4fa49f886732e548a90d4cc4a7045113028a7
Victor <lobovctoraraujo@gmail.com>
2026-06-17 22:44:36 -0300

    fix: centralização em um único .env

--- infra/prisma/seed/index.ts
+++ infra/prisma/seed/index.ts
@@ -1,4 +1,6 @@
-import 'dotenv/config';
+import { config } from 'dotenv';
+config({ path: resolve(__dirname, '../../../apps/api/.env') });
+import { resolve } from 'path';
 import {
   PrismaClient,
   Role,
