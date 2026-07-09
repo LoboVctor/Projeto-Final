cbd04c6e5618143a27874e0e97ea6a304e417de3
Victor <lobovctoraraujo@gmail.com>
2026-07-09 01:19:23 -0300

    Corrige imports incorretos (CardAluno->CardAlunoComponent, GraficoDiagnosticos->GraficoDiagnosticosComponent)

--- apps/web/projects/web/src/app/funcionalidades/turma/components/grafico-diagnosticos/grafico-diagnosticos.spec.ts
+++ apps/web/projects/web/src/app/funcionalidades/turma/components/grafico-diagnosticos/grafico-diagnosticos.spec.ts
@@ -1,16 +1,27 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { provideRouter } from '@angular/router';
+import { API_BASE_URL } from '../../../../nucleo/config/api.config';
 
-import { GraficoDiagnosticos } from './grafico-diagnosticos';
+import { GraficoDiagnosticosComponent } from './grafico-diagnosticos';
 
 describe('GraficoDiagnosticos', () => {
-  let component: GraficoDiagnosticos;
-  let fixture: ComponentFixture<GraficoDiagnosticos>;
+  let component: GraficoDiagnosticosComponent;
+  let fixture: ComponentFixture<GraficoDiagnosticosComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [GraficoDiagnosticos] }).compileComponents();
+      imports: [GraficoDiagnosticosComponent],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+        provideRouter([]),
+        { provide: API_BASE_URL, useValue: 'http://localhost:3000/api/v1' },
+      ] }).compileComponents();
 
-    fixture = TestBed.createComponent(GraficoDiagnosticos);
+    fixture = TestBed.createComponent(GraficoDiagnosticosComponent);
+    fixture.componentRef.setInput('turmaId', 'turma-teste-id');
     component = fixture.componentInstance;
     await fixture.whenStable();
   });
